6f447616c87c69f994eb1794831690831c378d7b
Egor <yegor.bashchinsky@lunalabs.uk>
2021-03-12 15:27:39 +0300

    feat: human readable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable( number ) {
+    let digits = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
+    let tenth = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' ]
+    let underHunded = [ '0', '1', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ]
+    if ( number <= 9 )
+        return digits[ number ];
+    if ( number <= 19 )
+        return tenth[ number % 10 ];
+    if ( number < 100 ) {
+        let tenCount = Math.floor( number / 10 );
+        let otherNumber = number % 10;
+        let result = underHunded[ tenCount ];
+        if ( otherNumber != 0 ) {
+            result += ' ' + toReadable( otherNumber );
+        }
+        return result;
+    }
+    if ( number <= 999 ) {
+        let hunderds = Math.floor( number / 100 );
+        let other = number % 100;
+        let result = digits[ hunderds ] + ' hundred';
+        if ( other != 0 )
+            result += ' ' + toReadable( other );
+
+        return result;
+    }
+
+    return 'cant parse';
 }
